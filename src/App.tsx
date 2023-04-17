0cf0484a8119ccfe22ffd901927d990712bbe513
rinatbul <rinatbuldybayev@gmail.com>
2023-04-17 21:46:13 +0600

    add loading status to component

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,43 @@ import {Orders} from "./Components/Orders/Orders";
 import {useEffect, useState} from "react";
 
 export type OrderType = {
-    id:number
-    key:string
-    is_ready:boolean
+    id: number
+    key: string
+    is_ready: boolean
 }
 
 function App() {
-    const [orders,setOrders] =useState<OrderType[]>([])
+    const [orders, setOrders] = useState<OrderType[]>([])
+    const [loading, setLoading] = useState<boolean>(false)
 
-    useEffect(()=>{
+    useEffect(() => {
+        setLoading(true)
         fetch('https://online-kezek-test-production-5624.up.railway.app/api/restaurants/1/orders/')
-            .then(response =>response.json())
-            .then(data => setOrders(data.orders))
-            .catch(error=>console.error(error))
-    },[])
+            .then(response => response.json())
+            .then(data => {
+                setLoading(false)
+                setOrders(data.orders)
+                setTimeout(()=>{
+                    setLoading(false)
+                },2000)
+            })
+            .catch(error => console.error(error))
+    }, [])
 
-    const completeOrders = orders.filter(order=>order.is_ready)
-    const inProgressOrders = orders.filter(order=>!order.is_ready)
+    const completeOrders = orders.filter(order => order.is_ready)
+    const inProgressOrders = orders.filter(order => !order.is_ready)
+
 
     return (
         <div className="App">
-            <Orders orders={inProgressOrders}/>
-            <Orders orders={completeOrders}/>
+            <Orders orders={inProgressOrders}
+                    title={'Готовятся'}
+                    status={'notReady'}
+                    isLoading={loading}/>
+            <Orders orders={completeOrders}
+                    title={'Готовы'}
+                    status={'ready'}
+                    isLoading={loading}/>
         </div>
     )
 }
--- src/Components/Orders/Orders.tsx
+++ src/Components/Orders/Orders.tsx
@@ -2,20 +2,43 @@ import s from "./Orders.module.css";
 import {OrderType} from "../../App";
 
 type OrdersPropsType = {
+    title: string
     orders: OrderType[]
+    status: 'ready' | 'notReady'
+    isLoading: boolean
+}
+
+const OrdersStyles = {
+    ready: {
+        backgroundColor: '#298000',
+        borderColor: '#298000'
+    },
+    notReady: {
+        backgroundColor: '#c25100',
+        borderColor: '#c25100'
+    }
 }
 
 export const Orders = (props: OrdersPropsType) => {
+
+    const orders = props.orders.length ? (
+        props.orders.map((order, index) => {
+            return <div className={s.item}>
+                <button key={index}
+                        style={{borderColor: `${OrdersStyles[props.status].borderColor}`}}>{order.key}</button>
+            </div>
+        })
+    ) : (<div>Not found</div>)
     return (
         <div className={s.wrapper}>
-            <div className={s.header}>Готовятся</div>
-            <div className={s.orders_wrapper}>
-                {props.orders.map((order, index) => {
-                    return <div className={s.item}>
-                        <button key={index}>{order.key}</button>
-                    </div>
-                })}
+            <div className={s.header}
+                 style={{backgroundColor: `${OrdersStyles[props.status].backgroundColor}`}}>
+                {props.title}
             </div>
+            {
+                props.isLoading ? <div>Loading...</div> : orders
+            }
+
         </div>
     )
 }
